0a14f5e624f7b07527328d75b346618e36fbc1cf
subramani-lab <ashoktorre@gmail.com>
2025-10-28 15:07:41 +0530

    Update and rename silver.erp_loc_a101.sql to silver.erp_loc_a101 &erp_px_cat_g1v2 .sql

--- sql-scripts/Silver_layer/silver.erp_loc_a101 &erp_px_cat_g1v2 .sql
+++ sql-scripts/Silver_layer/silver.erp_loc_a101 &erp_px_cat_g1v2 .sql
@@ -21,3 +21,35 @@ SELECT * FROM silver.erp_loc_a101
 select * from bronze.erp_loc_a101
 ----Data Standardization & Consistency-----------------
 select distinct CNTRY from silver.erp_loc_a101
+----------------------------------------------------
+
+#erp_px_cat_g1v2
+
+select * from bronze.erp_px_cat_g1v2
+-----------Checking unwanted spaces-----------------
+select ID from bronze.erp_px_cat_g1v2
+where ID != trim(ID)
+
+select CAT from bronze.erp_px_cat_g1v2
+where CAT != trim(CAT)
+
+select SUBCAT from bronze.erp_px_cat_g1v2
+where SUBCAT != trim(SUBCAT)
+
+select MAINTENANCE from bronze.erp_px_cat_g1v2
+where MAINTENANCE != trim(MAINTENANCE)
+------------Data standaridization & Consistency--------
+select distinct MAINTENANCE from bronze.erp_px_cat_g1v2
+
+------------------------------------------------------
+INSERT INTO SILVER.erp_px_cat_g1v2(
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE)
+select  
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE
+from bronze.erp_px_cat_g1v2
