339a54e410107a8f6bb3d4c6049d2d50abe423d2
subramani-lab <ashoktorre@gmail.com>
2025-10-28 14:57:00 +0530

    Update silver.erp_loc_a101.sql

--- sql-scripts/Silver_layer/silver.erp_loc_a101.sql
+++ sql-scripts/Silver_layer/silver.erp_loc_a101.sql
@@ -1,25 +1,23 @@
 ----Data Standardization & Consistency-----------------
 select distinct CNTRY from bronze.erp_loc_a101
-
 -------------------------------------------------------
+	
 INSERT INTO SILVER.erp_loc_a101(
 CID,
 CNTRY
 )
 select 
-REPLACE(CID, '-' ,'') AS CID,
+REPLACE(CID, '-' ,'') AS CID, --Replace '-' wiht '' spalce
 CASE WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
      WHEN TRIM(CNTRY) IN ('US','USA') THEN 'united states'
 	 WHEN TRIM(CNTRY) = '' OR CNTRY IS NULL THEN 'n/a'
 	 ELSE TRIM(CNTRY)
-END AS CNTRY
+END AS CNTRY --Normlize and handel missing or blank country codes
 from bronze.erp_loc_a101
 order by CID
-
-
+	
 ---------Data quality check--------------------------------
 SELECT * FROM silver.erp_loc_a101
 select * from bronze.erp_loc_a101
-
 ----Data Standardization & Consistency-----------------
 select distinct CNTRY from silver.erp_loc_a101
